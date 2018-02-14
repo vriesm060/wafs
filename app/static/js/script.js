31c5645032e033d9470ea8feb5913ff5d01d204c
Max de Vries <max.devries.1995@gmail.com>
2018-02-14 11:00:16 +0100

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -11,18 +11,53 @@
 
 	var routes = {
 		init: function () {
+			routes.getApi();
 			routes.findHash();
 		},
-		findHash: function () {
-			if (hash().length) sections.toggle(hash());
+		getApi: function () {
+			fetch ('https://kitsu.io/api/edge/anime')
+				.then(function (res, err) {
+					return res.json();
+				})
+				.then(function (res, err) {
 
-			window.addEventListener('hashchange', function () {
-				sections.toggle(hash());
-			}, false);
+					localStorage.setItem('data', JSON.stringify(res));
+					sections.insert(res.data);
+
+				})
+				.catch(function (err) {
+					console.log(err);
+				});
+		},
+		findHash: function () {
+			routie({
+				'home': function () {
+					sections.toggle('#home');
+				},
+				'anime': function () {
+					sections.toggle('#anime');
+				},
+				'episodes': function () {
+					sections.toggle('#episodes');
+				}
+			});
 		}
-	}
+	};
 
 	var sections = {
+		insert: function (data) {
+			console.log(data[0].attributes.abbreviatedTitles[0]);
+			for (var i = 0; i < data.length; i++) {
+				var span = document.createElement('SPAN');
+				span.setAttribute('data-bind', 'title' + i);
+				span.textContent = data[i].attributes.abbreviatedTitles[0];
+			}
+			/*var homeContent = {
+				titles: 
+			};
+
+			Transparency.render(document.querySelector('#home'), homeContent);*/
+		},
 		toggle: function (route) {
 			var section = document.querySelectorAll('section');
 			var current = document.querySelector(route);
@@ -35,10 +70,28 @@
 				}
 			});
 		}
-	}
+	};
 
-	var hash = function () { return window.location.hash; }
+	var hash = function () { return window.location.hash; };
 
 	app.init();
+
+
+
+	// Oude routes:
+	/*var routes = {
+		init: function () {
+			routes.findHash();
+		},
+		findHash: function () {
+			if (hash().length) sections.toggle(hash());
+
+			window.addEventListener('hashchange', function () {
+				sections.toggle(hash());
+			}, false);
+		}
+	}*/
+
+
 
 })();
