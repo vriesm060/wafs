54f10acc984d18dbe501906b5ad9beffdf03b646
Max de Vries <max.devries.1995@gmail.com>
2018-02-23 10:18:17 +0100

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,84 +5,128 @@
 
 	var app = {
 		init: function () {
-			routes.init();
+			router.init();
 		}
 	};
 
-	var routes = {
+	var router = {
 		init: function () {
-			routes.getApi();
-		},
-		getApi: function () {
-			fetch ('https://kitsu.io/api/edge/anime')
+
+			// Show loader:
+			utils.loader.show();
+
+			// Activate sort button:
+			utils.sortButton.el.addEventListener('click', function () {
+				collection.sortByRank();
+			}, false);
+
+			// Get data:
+			api.getData();
+
+			routie({
+				'home': function () {
+					templates.toggle('#home');
+				},
+				'anime': function () {
+					templates.toggle('#anime');
+				},
+				'details/:slug': function (slug) {
+					templates.toggle('#details');
+					collection.getDetails(slug);
+				},
+				'error': function () {
+					templates.toggle('#error');
+				}
+			});
+		}
+	};
+
+	var api = {
+		getData: function () {
+			return fetch ('https://kitsu.io/api/edge/anime')
 				.then(function (res, err) {
 					return res.json();
 				})
 				.then(function (res, err) {
 
-					localStorage.setItem('data', JSON.stringify(res.data));
-					sections.insert(res.data);
-					routes.findHash(res.data);
+					var map = res.data.map(function (item) {
+						return {
+							id: item.id,
+							attributes: {
+								'canonicalTitle': item.attributes.canonicalTitle,
+								'coverImage': item.attributes.coverImage,
+								'popularityRank': item.attributes.popularityRank,
+								'posterImage': item.attributes.posterImage,
+								'slug': item.attributes.slug,
+								'synopsis': item.attributes.synopsis
+							}
+						}
+					});
+
+					// Store the data in the storeData array in collection:
+					collection.storeData = map;
+
+					// Render the overview:
+					templates.render(map);
 
 				})
 				.catch(function (err) {
+					// Handle errors:
 					console.log(err);
+					
 				});
-		},
-		findHash: function (data) {
-			routie({
-				'home': function () {
-					sections.toggle('#home');
-				},
-				'anime': function () {
-					sections.toggle('#anime');
-				},
-				'episodes': function () {
-					sections.toggle('#episodes');
-				},
-				'details/:slug': function (slug) {
-					sections.toggle('#details');
-
-					var filterTitles = data.filter(function (item) {
-						if (item.attributes.slug === slug) return item;
-					});
-
-					var directives = {
-						posterImage: {
-							src: function () { return this.attributes.posterImage.small; }
-						},
-						title: {
-							text: function () { return this.attributes.titles.en_jp; }
-						},
-						synopsis: {
-							text: function () { return this.attributes.synopsis; }
-						}
-					}
-
-					Transparency.render(document.querySelector('#details'), filterTitles, directives);
-				}
-			});
 		}
 	};
 
-	var sections = {
-		insert: function (data) {
-			var mapTitles = data.map(function (item) {
-				return item;
+	var collection = {
+		storeData: [],
+		getDetails: function (slug) {
+			var details = this.storeData.filter(function (item) {
+				if (item.attributes.slug === slug) return item;
 			});
+			templates.renderDetail(details);
+		},
+		sortByRank: function () {
+			var sort = this.storeData.sort(function (a, b) {
+				return a.attributes.popularityRank - b.attributes.popularityRank;
+			});
+			templates.render(sort);
+		}
+	};
 
+	var templates = {
+		render: function (data) {
 			var directives = {
+				link: {
+					href: function (params) { return params.value + '/' + this.attributes.slug; }
+				},
+				posterImage: {
+					src: function () { return this.attributes.posterImage.small; }
+				},
 				title: {
-					text: function () {
-						return this.attributes.titles.en_jp;
-					},
-					href: function (params) {
-						return params.value + '/' + this.attributes.slug;
-					}
+					text: function () { return this.attributes.canonicalTitle; }
 				}
 			}
 
-			Transparency.render(document.querySelector('#anime-shows'), mapTitles, directives);
+			Transparency.render(document.querySelector('#anime-shows'), data, directives);
+
+			// Hide loader:
+			utils.loader.hide();
+		},
+		renderDetail: function (data) {
+			var directives = {
+				coverImage: {
+					src: function () { return this.attributes.coverImage.large; }
+				},
+				title: {
+					text: function () { return this.attributes.canonicalTitle; }
+				},
+				synopsis: {
+					text: function () { return this.attributes.synopsis; }
+				}
+			}
+
+			Transparency.render(document.querySelector('#details'), data, directives);
 		},
 		toggle: function (route) {
 			var section = document.querySelectorAll('section');
@@ -95,6 +139,21 @@
 					item.classList.remove('active');
 				}
 			});
+		}
+	};
+
+	var utils = {
+		loader: {
+			el: document.querySelector('#anime-shows'),
+			show: function () {
+				this.el.classList.add('show');
+			},
+			hide: function () {
+				this.el.classList.remove('show');
+			}
+		},
+		sortButton: {
+			el: document.querySelector('#sort')
 		}
 	};
 
